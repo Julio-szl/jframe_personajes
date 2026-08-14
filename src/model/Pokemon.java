70a116ca57e24ded5066811046d45b399af5390b
Julio-szl <julio.szl591@gmail.com>
2026-08-14 17:37:36 -0600

    Creacion de clase e inicio de creación de tipos

---
+++ src/model/Pokemon.java
@@ -0,0 +1,98 @@
+package model;
+
+public abstract class Pokemon {
+
+    String nombre;
+    int numeroPokemon;
+    int ps;
+    int ataque;
+    int defensa;
+    int velocidad;
+    String tipo;
+    String altura;
+    String categoria;
+    String peso;
+    String habilidad;
+    String genero;
+    String rutaImagen;
+
+    public Pokemon(String nombre, int numeroPokemon, int ps, int ataque, int defensa, 
+        int velocidad, String tipo, String altura, String categoria, String peso, String habilidad, 
+        String genero, String rutaImagen) {
+        this.nombre = nombre;
+        this.numeroPokemon = numeroPokemon;
+        this.ps = ps;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.velocidad = velocidad;
+        this.tipo = tipo;
+        this.altura = altura;
+        this.categoria = categoria;
+        this.peso = peso;
+        this.habilidad = habilidad;
+        this.genero = genero;
+        this.rutaImagen = rutaImagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getNumeroPokemon() {
+        return numeroPokemon;
+    }
+
+    public int getPs() {
+        return ps;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public String getHabilidad() {
+        return habilidad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+    
+    public abstract String verHabilidad();
+
+    public String mostrarFicha(){
+        return String.format(
+            "%s [%s] - Puntos de Salud: %d | Daño: %d",
+            nombre, categoria, ps, ataque
+        );
+    }
+
+}
---
+++ src/model/tipos/Normal.java
@@ -0,0 +1,50 @@
+package model.tipos;
+
+import model.Pokemon;
+
+public class Normal extends Pokemon{
+
+    int ataqueEspecial;
+    int defensaEspecial;
+    String elemento;
+    String debilidad;
+    
+    public Normal(String nombre, int numeroPokemon, int ps, int ataque, int defensa, int velocidad, String tipo,
+            String altura, String categoria, String peso, String habilidad, String genero, String rutaImagen,
+            int ataqueEspecial, int defensaEspecial, String elemento, String debilidad) {
+        super(nombre, numeroPokemon, ps, ataque, defensa, velocidad, tipo, altura, categoria, peso, habilidad, genero,
+                rutaImagen);
+        this.ataqueEspecial = ataqueEspecial;
+        this.defensaEspecial = defensaEspecial;
+        this.elemento = elemento;
+        this.debilidad = debilidad;
+    }
+
+    public int getAtaqueEspecial() {
+        return ataqueEspecial;
+    }
+
+    public int getDefensaEspecial() {
+        return defensaEspecial;
+    }
+
+    public String getElemento() {
+        return elemento;
+    }
+
+    public String getDebilidad() {
+        return debilidad;
+    }
+    
+
+    @Override
+    public String verHabilidad() {
+        return getNombre() + " tiene la habilidad: " + getHabilidad();
+    }
+
+    @Override
+    public String mostrarFicha() {
+        return super.mostrarFicha();
+    }
+
+}
